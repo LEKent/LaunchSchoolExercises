4e67adb3175648a6a4e44e4ebb28ac2c8b178b80
LEKent <leanneekent@gmail.com>
2016-10-19 09:18:26 -0600

    Add files via upload

---
+++ methods.rb
@@ -0,0 +1,28 @@
+#Exercise 1
+def greeting (name)
+	puts "Hi," + name
+end
+
+greeting ("Leanne")
+
+#Exercise 2
+#2, nil, "Joe", "four", nil
+
+#Exercise 3
+def multiply (a, b)
+	a * b
+end
+puts multiply 30, 3
+
+#Exercise 4
+# nothing because return is placed prior to puts command
+
+#Exercise 5
+def scream(words)
+	words = words + "!!!!"
+	puts words
+end
+
+scream ("Yippeeee")
+
+#6 there are the wrong number of argumentds in method called calculate_product
