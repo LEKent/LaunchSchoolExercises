12bb2b8899c5be832f86c36c2c1aeffde22065dc
LEKent <leanneekent@gmail.com>
2016-10-26 08:34:28 -0600

    Add files via upload

---
+++ arrays.rb
@@ -0,0 +1,32 @@
+#exercise 1
+=begin arr = [1, 3, 5, 7, 9, 11]
+arr.include?(3)
+=end 
+#exercise 2
+# arr = [[b ] [b, 2], [b, 3], [a, 1], [a, 2], [a, 3]]
+
+# arr = [["b"]], ["a", [1, 2, 3]]]
+
+arr = [["test", "hello", "world"], ["example", "mem"]]
+puts arr.last.first
+
+#Exercise 4
+=begin
+1. 3, 
+2. error
+3. 8
+
+
+#Exercise 5
+a = "e"
+b = "A"
+c = "nil"
+
+#Exercise 6
+names[3] = 'jody'
+
+#Exercise 7
+=end
+array = [1, 2, 3, 4, 5]
+array.each {|x| p x+2}
+p array
